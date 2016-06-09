381d36650c6d060478e2d05ac4c2e040a72edb71
Jay Hayes <ur@iamvery.com>
2016-06-09 13:05:06 -0500

    Configure ratchet

--- config/dev.exs
+++ config/dev.exs
@@ -20,7 +20,7 @@ config :pakex, Pakex.Endpoint,
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
       ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$}
+      ~r{web/templates/.*(eex|ratchet)$}
     ]
   ]
 
