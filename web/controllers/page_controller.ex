7411d4d99dd33d384e84f2d32e3c68783cbb776e
Jay Hayes <ur@iamvery.com>
2016-06-13 09:39:18 -0500

    Load all messages for page index

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule Pakex.PageController do
   use Pakex.Web, :controller
 
   def index(conn, _params) do
-    data = Pakex.Data.new
+    data = Pakex.Data.new |> Pakex.Data.scope(:message, Message.all)
     render conn, "index.html", data: data
   end
 end
