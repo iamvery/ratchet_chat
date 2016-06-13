7abbd21f7a913e0492f23852c2f7753b6f4baac2
Jay Hayes <ur@iamvery.com>
2016-06-13 09:47:02 -0500

    Load messages via mutator

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule Pakex.PageController do
   use Pakex.Web, :controller
 
   def index(conn, _params) do
-    data = Pakex.Data.new |> Pakex.Data.scope(:message, Message.all)
+    data = Pakex.Data.new |> Pakex.Data.scope(:message, Pakex.MessageMutator.all)
     render conn, "index.html", data: data
   end
 end
