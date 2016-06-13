89d894ea8d123b206b4baf7b6fc01566a55f786b
Jay Hayes <ur@iamvery.com>
2016-06-13 09:41:33 -0500

    Add function to add scoped data in a pakex data structure

--- lib/pakex/data.ex
+++ lib/pakex/data.ex
@@ -1,3 +1,8 @@
+# This would probably end up being part of Ratchet
 defmodule Pakex.Data do
   def new, do: %{}
+
+  def scope(data, property, property_data) do
+    Map.put(data, property, property_data)
+  end
 end
