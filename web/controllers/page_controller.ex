f8169c0bf1faa14e78d768a2b01a87f12a76c8dd
Jay Hayes <ur@iamvery.com>
2016-06-13 17:55:16 -0500

    Extract some private data functions

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,13 @@ defmodule Pakex.PageController do
   use Pakex.Web, :controller
 
   def index(conn, _params) do
-    data = Pakex.Data.new |> Pakex.Data.scope(:message, Pakex.MessageMutator.all)
     render conn, "index.html", data: data
   end
+
+  defp data do
+    Pakex.Data.new
+    |> Pakex.Data.scope(:message, all_messages)
+  end
+
+  defp all_messages, do: Pakex.MessageMutator.all
 end
