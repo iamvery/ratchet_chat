2557ae27469649d934d6d38f915a559f7732bfc5
Jay Hayes <ur@iamvery.com>
2016-06-13 09:47:45 -0500

    Update frontend on mutation

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -54,9 +54,13 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
+let channel = socket.channel("mutations", {})
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
+
+channel.on("create", payload => {
+  $(`li[data-prop=${payload.target}]`).parent().html(payload.view)
+})
 
 export default socket
