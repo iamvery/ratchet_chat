ed99ac5c8464dfd17871a2c0d627c3003e47cb65
Jay Hayes <ur@iamvery.com>
2016-06-13 09:45:51 -0500

    Define mutator for messages
    - query to load all messages
    - action to create new message

---
+++ web/mutators/message_mutator.ex
@@ -0,0 +1,11 @@
+defmodule Pakex.MessageMutator do
+  use Mutation, prop: :message, endpoint: Pakex.Endpoint, view: Pakex.PageView, template: "message.html"
+
+  query(:all) do
+    Message.all
+  end
+
+  action(:create) do
+    Message.add(params["body"])
+  end
+end
